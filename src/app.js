fa5fa4f402e8007c8eaa93e5cdf852aaec7df2b2
igor.oliveira <igor.oliveira@cocobambu.com>
2020-06-19 05:44:14 -0300

    first version of the application routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,56 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+const existsUrl = (request, response, next) => {
+  const { url } = request.body;
+  const condition = repository => repository.url == url;
+  const existsRepository = repositories.find(condition);
+  if(existsRepository) 
+    return response.status(403).json({ error: "repository already exists"});
+  request.urlRepository = url;
+  return next();
+}
+
+const existsRepository = (request, response, next) => {
+  const { id } = request.params;
+  const index = repositories.findIndex(repository => repository.id == id);
+  if(index < 0) 
+    return response.status(400).json({ error: "repository does not exists"});
+  request.indexRepository = index;
+  return next();
+}
+
+app.get("/repositories", (request, response) => 
+  response.status(200).json(repositories));
+
+app.post("/repositories", existsUrl, (request, response) => {
+  const { title, techs} = request.body;
+  const repository = { 
+    id: uuid(), likes: 0, title, url: request.urlRepository, techs };
+  repositories.push(repository);
+  return response.status(201).json(repository);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.put("/repositories/:id", existsRepository, (request, response) => {
+  const index = request.indexRepository;
+  let repository = repositories[index]; 
+  const { title, url, techs } = request.body;
+  const { id, likes } = repository;
+  repository = { id, title, url, techs, likes };
+  return response.status(200).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", existsRepository, (request, response) => {
+  const index = request.indexRepository;
+  repositories.splice(index, 1);
+  return response.status(204).send();
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
-
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", existsRepository, (request, response) => {
+  const index = request.indexRepository;
+  const repository = repositories[index];
+  repository.likes ++;
+  return response.status(200).json(repository);
 });
 
 module.exports = app;
